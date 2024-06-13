c7261c0a126f46fe4d08b6363df2a85f07f1762b
Pablo Nicolas Marin Gonzalez <103337571+Slylem0@users.noreply.github.com>
2024-06-12 20:58:32 -0500

    final
    ya casi es el final

--- rpg/utilities.py
+++ rpg/utilities.py
@@ -9,8 +9,8 @@ HEIGHT = 480
 
 def load_image(filename, transparent=False):
     try: image = pygame.image.load(filename)
-    except pygame.error, message:
-        raise SystemExit, message
+    except pygame.error: message = 'Cannot load image: %s' % filename
+    raise SystemExit, message
     image = image.convert()
     if transparent: 
         color = image.get_at((0,0)) 
